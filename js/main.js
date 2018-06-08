f8442207df55e761bab567b8cefffd9870978b17
Muthukumar kr <mk.kraam@gmail.com>
2018-06-08 15:50:21 +0800

    Update Js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
- 
+$(function() {
+    var startGame = function() {
+        var level = $('#level').val();
+        var totalRow = 2+level*2;
+        var totalBox = totalRow*totalRow;
+        for (let i = 0; i < totalBox; i++) {
+            $( "#game" ).add( "div" );
+            console.log(i)
+        }
+        console.log($( "#game" ));
+    }
+    $('#new').click(startGame)
+    startGame();
+    
+    
+    
+  });
