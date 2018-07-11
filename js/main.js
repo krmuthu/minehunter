99137b7c4419b0b7720bb58395c8cd0fbb068711
Muthukumar kr <mk.kraam@gmail.com>
2018-07-11 18:32:40 +0800

    Update Bomb generation

--- js/main.js
+++ js/main.js
@@ -1,17 +1,19 @@
 $(function() {
     var startGame = function() {
         var level = $('#level').val();
-        var totalRow = 2+level*2;
+        var totalRow = 5+level*4;
         var totalBox = totalRow*totalRow;
+        $( "#game").css('width', 30*totalRow+'px');
+        $( "#game" ).html( "" );
         for (let i = 0; i < totalBox; i++) {
-            $( "#game" ).add( "div" );
-            console.log(i)
+            var isBomb = Math.floor(Math.random()*5);
+            $( "#game" ).append( "<div data-bomb="+isBomb+"></div>" );
         }
-        console.log($( "#game" ));
     }
+    
+    $( "#game" ).click('div',function clickFn(e) {
+        console.log(e.target)
+    })
     $('#new').click(startGame)
     startGame();
-    
-    
-    
-  });
+});
