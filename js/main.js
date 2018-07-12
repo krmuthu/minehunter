41a2c276e6e3a5a83ab4efb3920a8da608ec64de
Muthukumar kr <mk.kraam@gmail.com>
2018-07-12 20:31:31 +0800

    Update neighbour logic

--- js/main.js
+++ js/main.js
@@ -1,18 +1,63 @@
 $(function() {
+    var num;
+    var totalBox;
     var startGame = function() {
         var level = $('#level').val();
-        var totalRow = 5+level*4;
-        var totalBox = totalRow*totalRow;
-        $( "#game").css('width', 30*totalRow+'px');
+        num = 5+level*4;
+        totalBox = num*num;
+        $( "#game").css('width', 30*num+'px');
         $( "#game" ).html( "" );
         for (let i = 0; i < totalBox; i++) {
             var isBomb = Math.floor(Math.random()*5);
-            $( "#game" ).append( "<div data-bomb="+isBomb+"></div>" );
+            if(isBomb){
+                $( "#game" ).append( '<div data-id="'+i+'" ></div>' );
+            }else {
+                $( "#game" ).append( '<div data-id="'+i+'" data-bomb="1"></div>' );
+            }
+            
         }
+    }
+    var getNeighbour = function getNeighbour(id) {
+        var neighbour = []
+        if(id%num != 0){
+            neighbour.push(id-1);
+            if(id > 8){
+                neighbour.push(id-num-1);
+            }
+            if(id < totalBox-num){
+                neighbour.push(id+num-1);
+            }
+        }
+        if(id%num != (num-1)){
+            neighbour.push(id+1);
+            if(id > 8){
+                neighbour.push(id-num+1);
+            }if(id < totalBox-num){
+                neighbour.push(id+num+1);
+            }
+        }
+        if(id > 8){
+            neighbour.push(id-num);
+        }
+        if(id < totalBox-num){
+            neighbour.push(id+num);
+        }
+        
+        return neighbour;
+    }
+    var checkForBomb = function checkForBomb(id) {
+    
     }
     
     $( "#game" ).click('div',function clickFn(e) {
-        console.log(e.target)
+        if($(e.target).data('bomb') == 1) {
+            console.log('you clicked on bomb');
+        } else {
+            var id = $(e.target).data('id');
+            console.log(id)
+            getNeighbour(id)
+
+        };
     })
     $('#new').click(startGame)
     startGame();
